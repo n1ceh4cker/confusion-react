fa25d5d409767a9fce4cb98ffb79da6ce4c85c57
n1ceakhtar <niceakhtar21@gmail.com>
2020-08-25 18:10:40 +0530

    Single Page Applications Part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,52 +1,61 @@
 import React from 'react'
-import { Card, CardImg, CardTitle, CardBody, CardText } from 'reactstrap'
+import { Card, CardImg, CardTitle, CardBody, CardText, Breadcrumb, BreadcrumbItem } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 const RenderDish = ({ dish })=>{
-        return(
-            <div className="col-12 col-md-5 m-1">
-                <Card key={dish.id}>
-                    <CardImg src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
+        return(   
+            <Card key={dish.id}>
+                <CardImg src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>            
         )
 }
                              
 const RenderComments = ({ comments }) =>{
-        return( 
-            <div className="col-12 col-md-5 m-1">
-                <h5 className="pb-2">Comments</h5>
-                <ul className="list-unstyled">
-                {
-                    comments.map(comment =>{
-                        return(
-                        <li key={comment.id}>
-                            <div className="pb-2">
-                                {comment.comment}
-                            </div>
-                            <div className="pb-2">
-                                -- {comment.author}, {new Intl.DateTimeFormat('en-us', {year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}
-                            </div>
-                        </li>)
-                    })
-                }
-                </ul>    
-            </div>   
+        return(       
+            <ul className="list-unstyled">
+            {
+                comments.map(comment =>{
+                    return(
+                    <li key={comment.id}>
+                        <div className="pb-3">
+                            {comment.comment}
+                        </div>
+                        <div className="pb-3">
+                            -- {comment.author}, {new Intl.DateTimeFormat('en-us', {year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}
+                        </div>
+                    </li>)
+                })
+            }
+            </ul>       
         )
     }
-const DishDetail = ({ dish }) =>{
+const DishDetail = ({ dish, comments }) =>{
         return (
-            dish!=null?
-            (<div className="container">
+            <div className="container">
                 <div className="row">
-                    <RenderDish dish={dish} />
-                    <RenderComments comments={dish.comments} />
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu" >Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{dish.name}</h3>
+                        <hr />
+                    </div>
                 </div>
-            </div>):
-            (<div></div>)
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <h5 className="pb-3">Comments</h5>
+                        <RenderComments comments={comments} />
+                    </div>
+                </div>
+            </div>
         )
 }
 
