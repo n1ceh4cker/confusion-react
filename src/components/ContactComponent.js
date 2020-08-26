cca6386163f70da8a9164e11485d950e5ac58611
n1ceakhtar <niceakhtar21@gmail.com>
2020-08-26 19:53:50 +0530

    Controlled Form Validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Breadcrumb, BreadcrumbItem, Form, FormGroup, Label, Col, Input, Button } from 'reactstrap'
+import { Breadcrumb, BreadcrumbItem, Form, FormGroup, Label, Col, Input, Button, FormFeedback } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
 class Contact extends Component {
@@ -10,19 +10,57 @@ class Contact extends Component {
         telnum:'',
         agree: false,
         contactType: 'Tel.',
-        feedback: ''
+        feedback: '',
+        touched: {
+            firstname: false,
+            lastname: false,
+            telnum: false,
+            email: false
+        }
     }
     handleInputChange = (e) =>{
         const value = e.target.type === 'checkbox'? e.target.checked : e.target.value 
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: value
         })
     }
     handleSubmit = (e) =>{
         e.preventDefault()
         alert('Contact information: ' + JSON.stringify(this.state))
     }
+    handleBlur = (field) => (e) => {
+        this.setState({
+            touched: {...this.state.touched, [field]: true }
+        })
+    }
+    validate = (firstname, lastname, telnum, email) =>{
+        const errors = {
+            firstname: '',
+            lastname: '',
+            telnum: '',
+            email: ''
+        }
+        if(this.state.touched.firstname && firstname.length < 3) 
+            errors.firstname = 'First name should be atleast 3 characters long'
+        else if(this.state.touched.firstname && firstname.length > 10) 
+            errors.firstname = 'First name should be atmost 10 characters long'
+
+        if(this.state.touched.lastname && lastname.length < 3) 
+            errors.lastname = 'Last name should be atleast 3 characters long'
+        else if(this.state.touched.lastname && lastname.length > 10) 
+            errors.lastname = 'Last name should be atmost 10 characters long'
+
+        const tel = /^\d{10}$/
+        if(this.state.touched.telnum && !tel.test(telnum))
+            errors.telnum = 'Tel number should be 10 digits only'
+        const mail = /^\S+@\S+\.\S+$/
+        if(this.state.touched.email && !mail.test(email))
+            errors.email = 'Enter a valid email'
+        return errors
+    }
     render(){
+        const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email)
+        console.log(errors)
         return (
             <div className="container">
                 <div className="row">
@@ -73,7 +111,11 @@ class Contact extends Component {
                                 <Col md={10}>
                                     <Input type="text" name="firstname" id="firstname" placeholder="First Name"
                                         value={this.state.firstname}
+                                        valid={errors.firstname===''}
+                                        invalid={errors.firstname!==''}
+                                        onBlur={this.handleBlur('firstname')}
                                         onChange={this.handleInputChange} />
+                                <FormFeedback>{errors.firstname}</FormFeedback>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -81,7 +123,11 @@ class Contact extends Component {
                                 <Col md={10}>
                                     <Input type="text" name="lastname" id="lastname" placeholder="Last Name"
                                         value={this.state.lastname}
-                                        onChange={this.handleInputChange}/>
+                                        valid={errors.lastname===''}
+                                        invalid={errors.lastname!==''}
+                                        onBlur={this.handleBlur('lastname')}
+                                        onChange={this.handleInputChange} />
+                                <FormFeedback>{errors.lastname}</FormFeedback>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -89,7 +135,11 @@ class Contact extends Component {
                                 <Col md={10}>
                                     <Input type="tel" name="telnum" id="telnum" placeholder="Tel. Number"
                                         value={this.state.telnum}
-                                        onChange={this.handleInputChange} />                                
+                                        valid={errors.telnum===''}
+                                        invalid={errors.telnum!==''}
+                                        onBlur={this.handleBlur('telnum')}
+                                        onChange={this.handleInputChange} /> 
+                                    <FormFeedback>{errors.telnum}</FormFeedback>                               
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -97,7 +147,11 @@ class Contact extends Component {
                                 <Col md={10}>
                                     <Input type="email" name="email" id="email" placeholder="Email"
                                         value={this.state.email}
+                                        valid={errors.email===''}
+                                        invalid={errors.email!==''}
+                                        onBlur={this.handleBlur('email')}
                                         onChange={this.handleInputChange} />
+                                    <FormFeedback>{errors.email}</FormFeedback>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
